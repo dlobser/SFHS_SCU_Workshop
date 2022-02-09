937d06185f5efe6e7a1f9f4a01307eff6cd52b0b
dlobser <davidlobser@yahoo.com>
2022-02-09 12:09:10 -0700

    rings within rings

--- Assets/Scripts/Orbit2.cs
+++ Assets/Scripts/Orbit2.cs
@@ -13,22 +13,34 @@ public class Orbit2 : MonoBehaviour
     public float amplitudeX = 1;
     public float amplitudeY = 1;
 
+    public bool rebuildOnStart = false;
+
     void Start()
     {
+       if(rebuildOnStart){
+           Rebuild();
+       }
+    }
+
+    public void Rebuild(){
         for (int i = 0; i < amount; i++)
         {
             GameObject element = Instantiate(spiralElement, transform);
+            if(element.GetComponent<Orbit2>()!=null){
+                element.GetComponent<Orbit2>().Rebuild();
+            }
         }
     }
 
     void Update()
     {
-        for (int i = 0; i < amount; i++)
+        for (int i = 0; i < transform.childCount; i++)
         {
             Transform elementTransform = transform.GetChild(i);
+            float offset = ((float)i/(float)amount) * Mathf.PI * 2;
             elementTransform.localPosition = new Vector3(
-                Mathf.Cos(i + Time.time * Mathf.PI * 2 * speedX) * amplitudeX,
-                Mathf.Sin(i + Time.time * Mathf.PI * 2 * speedY) * amplitudeY,
+                Mathf.Cos(offset + Time.time * Mathf.PI * 2 * speedX) * amplitudeX,
+                Mathf.Sin(offset + Time.time * Mathf.PI * 2 * speedY) * amplitudeY,
                 0);
         }
 
